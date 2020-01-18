dc49b72276df7ba6bbc8f214c9afdc8d8c415288
Diana Ponce <dianap2341@gmail.com>
2020-01-18 23:44:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-count=0 
-while count < array.length do 
- if value_to_find == array[count]
- return count
- end
+  count=0 
+  while count < array.length do 
+   if value_to_find == array[count]
+   return count
+end
  count+=1
 end
 end
